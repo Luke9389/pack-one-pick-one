b1d44598e2b7f775e87850b6e46c9d561de3c7cf
Sam Jespersen <heyheysamj@gmail.com>
2020-04-16 23:19:47 -0700

    more reorg + express

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import RatingPage from '../containers/RatingPage';
+import React, { useEffect } from 'react';
+// import RatingPage from '../containers/RatingPage';
 import '../../reset.css';
 
 export default function App() {
-  return <RatingPage />;
+
+  //USE HOOKS FOR EXPRESS API CALL
+
+  console.log('APP');
+
+  useEffect(() => {
+    async function getData() {
+      const data = await fetch('/api');
+      console.log(data.json());
+    }
+    getData();
+  }, []);
+
+  return <h1>sup</h1>;
 }
 
